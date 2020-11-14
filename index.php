91fbc107df120440e7917d90a01def846a15aa0c
rgruenberg <rebeccagruenberg@gmail.com>
2020-11-14 16:06:50 -0500

    Added favicon

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     
 <title>QuickCard - Home</title>
+
 <?php include 'includes/include_BS.php';?>
 </head>
 <body>
