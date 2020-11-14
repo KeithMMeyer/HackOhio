f952abedc0cc69bb93f87ebbdb45065db1852be6
rgruenberg <rebeccagruenbrg@gmail.com>
2020-11-14 01:00:48 -0500

    Fixed include links

--- sets.php
+++ sets.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html>
+
 <head>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script>var front= true;</script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <script>
 function flipCard() {
-  $("#currCard").html("Back Text");
+    if (front){
+        $("#currCard").html("Back Text");
+        front=false;
+    }
+    else{
+        $("#currCard").html("Front Text");
+        front=true;
+    }
 }
 </script>
 <title>Page Title</title>
@@ -23,7 +32,7 @@ function flipCard() {
   <li class="list-group-item">   Set 5   </li>
 </ul>
     </div>
-    <div class="card" style= "margin:auto"  align="center" style="width: 18rem;" onclick=flipCard();>
+    <div class="card" style= "margin:auto"; align="center" style="width: 18rem;" onclick=flipCard();>
         <br>
         <br>
         <br>
